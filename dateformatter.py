ec80866b167176b7ee9194370ef4211a1ea79cc1
adambozzo1 <35498211+adambozzo1@users.noreply.github.com>
2019-07-11 14:22:12 -0400

    Add files via upload
    added proper functionality

--- dateformatter.py
+++ dateformatter.py
@@ -1,9 +1,16 @@
 
-#!/usr/bin/python
+#program to correct date format for json files in order to import into robo3t
 
 import sys
+import re 
 
 fileName = sys.argv
-file = open(str(fileName[1]), "w+")
-file.write("heyyy")
-file.close()
+fileR = open(str(fileName[1]), "r")
+newlines = ""
+for line in fileR:
+    newlines += re.sub("(\+0000+)", "", line)
+
+fileR.close()
+fileW = open("results.txt", "w+")
+fileW.write(newlines)
+fileW.close()
